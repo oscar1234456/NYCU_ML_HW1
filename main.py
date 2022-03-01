4a3b0ccf10260af72bc7cfb32dd6035dbe8c7c46
oscar1234456 <chen.oscar@hotmail.com>
2022-03-01 14:14:05 +0800

    [Add]complete lse (lack of total loss)

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from matplotlib import pyplot as plt
 
 from config import constant
 from data_process import dataset
-from method.lse import lse
-
+from method import lse
 
 file_name = input("The file name:")
-n = input("n:")
-lam = input("lambda:")
+n = int(input("n:"))
+lamb = int(input("lambda:"))
 test_data = dataset.Dataset(constant.DATA_FILE_PATH, file_name)
 x1 = test_data.get_data_x()
-A = test_data.get_design_matrix(int(n))
+A = test_data.get_design_matrix(n)
 b = test_data.get_data_y()
+
+w = lse(A, b, lamb)
+print()
--- method/lse.py
+++ method/lse.py
@@ -1,11 +1,16 @@
 import numpy as np
 
+from linear_al.inverse import inverse
 
 
-
-def lse(A:np.ndarray , b, lamb):
+def lse(A, b, lamb):
+    # maybe A is not a symmetric matrix
+    # but, A.T@A should be a symmetric square matrix
+    # Moreover, A.T@A + lambdaI is a positive matrix, invertible
     A_row_len, A_col_len = A.shape
+    lambI = lamb * np.identity(A_col_len)  # notice: A.T@A is (A_col_len, A_col_len)
     # TODO: w = inverse(A.T@A+lambI)@A.T@b (closed form)
+    w = inverse(A.T@A+lambI)@A.T@b
     # TODO: cal loss
     # TODO: return w, loss
-    pass
+    return w
