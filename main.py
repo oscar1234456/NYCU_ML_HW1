f6a3467fb1f94e1c24d41da21722caa9be1b24ed
oscar1234456 <chen.oscar@hotmail.com>
2022-03-01 16:29:35 +0800

    [Add] preety plot for newthon

--- main.py
+++ main.py
@@ -7,19 +7,17 @@ from method import lse, newthon
 from print_process import pretty_output, pretty_plot
 
 file_name = input("The file name:")
-n = int(input("n:"))
-lamb = int(input("lambda:"))
-test_data = dataset.Dataset(constant.DATA_FILE_PATH, file_name)
-test_datapoint_x = test_data.get_data_x()
-A = test_data.get_design_matrix(n)
-test_datapoint_b = test_data.get_data_y()
+while True:
+    n = int(input("n:"))
+    lamb = int(input("lambda:"))
+    test_data = dataset.Dataset(constant.DATA_FILE_PATH, file_name)
+    test_datapoint_x = test_data.get_data_x()
+    A = test_data.get_design_matrix(n)
+    test_datapoint_b = test_data.get_data_y()
 
 
-newthon_w, newthon_loss = newthon(A, test_datapoint_b)
-
-# lse_w, lse_loss = lse(A, test_datapoint_b, lamb)
-# pretty_output(lse_w, lse_loss, method="lse")
-#
-# pretty_plot(test_datapoint_x, test_datapoint_b, lse_w)
-pretty_output(newthon_w, newthon_loss, method="newthon")
-pretty_plot(test_datapoint_x, test_datapoint_b, newthon_w)
+    lse_w, lse_loss = lse(A, test_datapoint_b, lamb)
+    newthon_w, newthon_loss = newthon(A, test_datapoint_b)
+    pretty_output(lse_w, lse_loss, method="lse")
+    pretty_output(newthon_w, newthon_loss, method="newthon")
+    pretty_plot(test_datapoint_x, test_datapoint_b, lse_w, newthon_w)
--- print_process/pretty_plot/pretty_plot.py
+++ print_process/pretty_plot/pretty_plot.py
@@ -2,14 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def pretty_plot(test_datapoint_x: np.ndarray, test_datapoint_b, w):
-    n = w.shape[0]
+def pretty_plot(test_datapoint_x: np.ndarray, test_datapoint_b, lse_w, newthon_w):
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    fig.suptitle('Fitting Curve')
+    n = lse_w.shape[0]
     test_datapoint_x_min = test_datapoint_x.min()
     test_datapoint_x_max = test_datapoint_x.max()
-    x_space = np.linspace(test_datapoint_x_min-1, test_datapoint_x_max+1, 500)
+    x_space = np.linspace(test_datapoint_x_min-1, test_datapoint_x_max+1)
     y = np.zeros(len(x_space))
-    plt.scatter(test_datapoint_x, test_datapoint_b, c = "red")
+    ax1.scatter(test_datapoint_x, test_datapoint_b, c = "red")
+    ax2.scatter(test_datapoint_x, test_datapoint_b, c="red")
     for i in range(n):
-        y = y + w[i] * np.power(x_space, i)
-    plt.plot(x_space, y, c="black")
+        y = y + lse_w[i] * np.power(x_space, i)
+    ax1.plot(x_space, y, c="black")
+    ax1.set_title("LSE")
+    y = np.zeros(len(x_space))
+    for i in range(n):
+        y = y + newthon_w[i] * np.power(x_space, i)
+    ax2.plot(x_space, y, c="black")
+    ax2.set_title("Newthon's method")
+    fig.tight_layout()
+
     plt.show()
